e3a1fbaca7d94052c92adac7eeec3c2ce174470d
EugeneLyutovskiy09 <61806687+EugeneLyutovskiy09@users.noreply.github.com>
2020-08-14 11:18:47 +0200

    Update SunRaysLT.cs

--- SunRaysLT.cs
+++ SunRaysLT.cs
@@ -19,23 +19,20 @@ public class Lifetime : MonoBehaviour
 
     void Update()
     {
-        int analogValueT = manager.analogRead(AnalogPin.A0);
-        int analogValuePh = manager.analogRead(AnalogPin.A5);
+        int analogValueT = manager.analogRead(AnalogPin.A0); // getting value from the thermoresistor (T-sensor)
+        int analogValuePh = manager.analogRead(AnalogPin.A5); // getting value from the photoresistor (Photosensor)
         var main = ps.main;
-        main.startLifetime = 1;
-        //float mappedT = analogValueT / 100;
+        main.startLifetime = 1; // lifetime parameter is set to a constant value 
 
-        float mappedPh = (analogValuePh - 0) * (7f - 0f) / (490f - 0f) + 0.1f;
-        //float value = 0.5f - mappedPh;
-           
-            main.startSpeed = mappedPh;
+        float mappedPh = (analogValuePh - 0) * (7f - 0f) / (490f - 0f) + 0.1f; // mapping the photosensor analig value onto the range (0 - 7), as we want start speed parameter to be changed within this figures 
 
-        // float mappedPh = (analogValuePh - 0) * (2f - 0.1f) / (250f - 0f) + 0.1f;
-        //main.startLifetime = mappedPh;
+            main.startSpeed = mappedPh; // setting the mapped photosensor analog value into the start speed parameter of the particle system. Gradually increasing/decreasng size of the sun rays when photosensor indicates rising/falling intensity 
 
-        float mappedT = (analogValueT - 0) * (1.0f - 0.1f) / (1000f - 0f) + 0.1f;
-        float valueColour = 1 - mappedT;
-        main.startColor = new Color(1, valueColour, 0, 1);
+        float mappedT = (analogValueT - 0) * (1.0f - 0.1f) / (1000f - 0f) + 0.1f; // mapping Tsensor analog value onto the range (0-0.1)
+        
+        float valueColour = 1 - mappedT; // we need the opposite value for the needed shade in the green channel 
+        
+        main.startColor = new Color(1, valueColour, 0, 1); // setting values into the green channel in the RGBA parameters in order to change the colours of the rays towards red colour when temp goes up  
 
     }
 }
