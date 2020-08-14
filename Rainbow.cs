46b2a1a755c3f560b4ab50b560724b009bd49195
EugeneLyutovskiy09 <61806687+EugeneLyutovskiy09@users.noreply.github.com>
2020-08-14 10:52:45 +0200

    Update Rainbow.cs

--- Rainbow.cs
+++ Rainbow.cs
@@ -18,16 +18,16 @@ public class Rainbow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int analogValueT = manager.analogRead(AnalogPin.A0);
-        int analogValuePh = manager.analogRead(AnalogPin.A5);
+        int analogValueT = manager.analogRead(AnalogPin.A0); // getting value from the t-sensor
+        int analogValuePh = manager.analogRead(AnalogPin.A5); // getting value from the photoresistor (photosensor) 
         var main = ps.main;
         if (analogValueT > 90 && analogValuePh > 170 && analogValuePh < 230)
         {
-            main.startLifetime = 1.66f;
-        }
-        else
+            main.startLifetime = 1.66f; // rainbow appears only in strict conditions: when it rains and when the clouds are not thik enough and the sun rays can penetrate them. 
+        else // No rainbow is possible when it snows, so temp also needs to be taken into account
         {
-            main.startLifetime = 0f;
+            main.startLifetime = 0f;   
+        }
         }
     }
 }
